3c2198e58ca9ce939ab52bf597ef8f4e13b0f31d
Victor Lima de Oliveira <victorlima.oliveira70@gmail.com>
2025-07-11 17:36:36 -0300

    Inicio do projeto e configurações iniciais

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e0218a',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
